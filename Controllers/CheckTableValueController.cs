666e77fec7536498d4faf1719c33135142a2c3bf
vinaynaidu9798-a11y <vinaynaidu9798@gmail.com>
2026-02-27 11:19:49 +0530

    Added api versions

--- Controllers/CheckTableValueController.cs
+++ Controllers/CheckTableValueController.cs
@@ -6,7 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 namespace FieldMetadataAPI.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/check-table-value")]
+    [ApiVersion("1.0")]
+    [Produces("application/json")]
     public class CheckTableValueController : ControllerBase
     {
         private readonly ICheckTableValueService _service;
@@ -25,6 +27,7 @@ namespace FieldMetadataAPI.Controllers
         /// Example:
         /// api/CheckTableValue?tableName=T134
         /// </summary>
+      
         [HttpGet]
         public async Task<IActionResult> Get(
             [FromQuery] string tableName)
--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ Log.Logger = new LoggerConfiguration()
 
 builder.Host.UseSerilog();
 
+
 // Add services to the container
 builder.Services.AddControllers();
 
